029d56e52a4e8ac32fbf7ff7eecab07d85140190
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2021-05-26 18:32:26 -0500

    set up home directory

--- src/backend/routes.js
+++ src/backend/routes.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// const resetDB = require("../config/scripts/populateDB")
+const resetDB = require("../../config/scripts/populateDB")
 
 const Killer = require("./schema/Killer");
 const Survivor = require("./schema/Survivor");
@@ -8,6 +8,23 @@ const Survivor = require("./schema/Survivor");
 const express = require("express");
 const router = express.Router();
 
-// routes here:
+// completely resets your database.
+// really bad idea irl, but useful for testing
+router.route("/reset")
+    .get((_req, res) => {
+        resetDB(() => {
+            res.status(200).send({
+                message: "Data has been reset."
+            });
+        });
+    });
+
+router.route("/")
+    .get((_req, res) => {
+        console.log("GET /");
+        res.status(200).send({
+            data: "App is running."
+        });
+    });
 
 module.exports = router;
