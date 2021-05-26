ce8fb90d97295ee4a0c088d1e516c5f838e25034
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2021-05-26 18:07:52 -0500

    rename backend directory

---
+++ src/backend/routes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// const resetDB = require("../config/scripts/populateDB")
+
+const Killer = require("./schema/Killer");
+const Survivor = require("./schema/Survivor");
+
+const express = require("express");
+const router = express.Router();
+
+// routes here:
+
+module.exports = router;
